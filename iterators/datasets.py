aff05858289d696284a99482b0e242c247465694
anonymous <anonymous@example.com>
2019-06-06 15:16:09 -0400

    Update from upstream

--- iterators/datasets.py
+++ iterators/datasets.py
@@ -5,6 +5,7 @@ import random
 import os
 import numpy as np
 import torch
+from scipy import io
 
 from torch.utils.data import Dataset
 from PIL import Image
@@ -245,3 +246,39 @@ class KMnistDatasetOneHot(datasets.KMNIST):
         y_batch_onehot = torch.eye(10)[y_batch]
         y_batch_onehot = y_batch_onehot.float()
         return x_batch, y_batch_onehot
+
+class OxfordFlowers102Dataset(Dataset):
+    def __init__(self,
+                 root,
+                 transforms_=None,
+                 mode='train',
+                 attrs=[],
+                 missing_ind=False):
+        self.transform = transforms.Compose(transforms_)
+        ids = np.arange(1, 8189+1)
+        indices = np.arange(0, len(ids))
+        rnd_state = np.random.RandomState(0)
+        rnd_state.shuffle(indices)
+        labels = io.loadmat('%s/imagelabels.mat' % root)['labels'].flatten()-1
+        # Shuffle both ids and labels with the same indices.
+        labels = labels[indices]
+        ids = ids[indices]
+        if mode == 'train':
+            # Training set is first 90%.
+            self.ids = ids[0:int(len(ids)*0.9)]
+            self.labels = labels[0:int(len(ids)*0.9)]
+        else:
+            # Valid set is last 10%.
+            self.ids = ids[int(len(ids)*0.9)::]
+            self.labels = labels[int(len(ids)*0.9)::]
+        self.root = root
+
+    def __getitem__(self, index):
+        jpg_name = "image_" + str(self.ids[index]).zfill(5) + ".jpg"
+        filepath = "%s/jpg/%s" % (self.root, jpg_name)
+        img = self.transform(Image.open(filepath))
+        label = torch.eye(102)[self.labels[index]].float()
+        return img, label
+
+    def __len__(self):
+        return len(self.ids)
